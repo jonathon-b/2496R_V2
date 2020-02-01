406e0d9f7e8217b1d10c7d1790ff3f2c43ddea52
dangthanhan507 <45112823+dangthanhan507@users.noreply.github.com>
2020-01-31 18:02:02 -0800

    Update config.cpp

--- src/custom_src/config.cpp
+++ src/custom_src/config.cpp
@@ -1,5 +1,5 @@
 #include "custom/config.hpp"
-
+#include "classes/PID.hpp"
 #define POT_TO_DEGREES 0
 
 Controller control(E_CONTROLLER_MASTER);
@@ -16,11 +16,42 @@ Motor mtr_rollL(5, E_MOTOR_GEARSET_18, false, E_MOTOR_ENCODER_COUNTS);
 Motor mtr_rollR(11, E_MOTOR_GEARSET_18, true, E_MOTOR_ENCODER_COUNTS);
 
 //encoder(port,adjacent_port, reverse)
-ADIEncoder enc_l(4,5,false);
-ADIEncoder enc_r(6,7,false);
 
 //ADI(port)
 ADIAnalogIn pot(1);
-ADIDigitalIn limit(3);
+ADIDigitalIn limit(7);
 ADIDigitalIn bump(2);
+ADIEncoder enc_r(3,4,true);
+ADIEncoder enc_l(5,6,false);
 ADIAnalogIn line(8);
+
+Imu imu(12);
+
+int pot_offset = 0;
+
+double preset_heights[3] = {LIFTH_BOTTOM, LIFTH_SMALLTOWER, LIFTH_BIGTOWER};
+PID autolift(KP_LIFT, KI_LIFT, KD_LIFT);
+int index_heights = 0;
+double lift_value = 0;
+void pid_lift() {
+
+
+  if(control.get_digital_new_press(E_CONTROLLER_DIGITAL_R1)) {
+    index_heights++;
+    index_heights = fmin(index_heights , sizeof(preset_heights) / sizeof(*preset_heights) - 1 );
+  }
+
+  if(control.get_digital_new_press(E_CONTROLLER_DIGITAL_L1)) {
+    index_heights--;
+    index_heights = fmax(index_heights , 0);
+  }
+
+  autolift.Calculate(preset_heights[index_heights], pot.get_value()-pot_offset, LIMIT_LIFT, LIFT_MAX);
+  if(std::abs(autolift.error) > 20) lift_value = -autolift.Calculate(preset_heights[index_heights], pot.get_value()-pot_offset, LIMIT_LIFT, LIFT_MAX) + GRAVFF;
+
+  mtr_lift.move_voltage(lift_value); // it move gamer arm
+  if(index_heights == 0 && std::abs(autolift.error) < 15) mtr_lift.move_velocity(0);
+  //target = preset_heights[index_heights]
+
+
+}
